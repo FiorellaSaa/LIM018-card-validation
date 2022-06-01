3f681bc85eb06962d37a678e987775c6f8160bc6
FiorellaSaa <fsaavedras.s@hotmail.com>
2022-06-01 11:29:09 -0500

    primerCommit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
-console.log(validator);
+let creditCardNumber = document.getElementById("creditCardNumber");
+let btnValidar = document.getElementById("btnValidar");
+let mensaje = document.getElementById("mensaje");
+
+
+btnValidar.addEventListener("click", (e) => {
+   e.preventDefault()
+
+    if(validator.isValid (creditCardNumber.value)){
+        mensaje.innerHTML = "Tu tarjeta es válida";
+    } else{
+        mensaje.innerHTML = `Tu tarjeta es inválida ${validator.maskify(creditCardNumber.value)}, ingrese otro número`;
+
+    }
+});
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,33 @@
 const validator = {
   // ...
-};
+  isValid:(creditCardNumber)=>{
+    const length = creditCardNumber.length;
+    const reverNumber = creditCardNumber.split("").reverse();
+    //console.log(reverNumber);
+
+    let sumaTotal = 0;
+    for(let i = 0; i < length; i++){
+      let newNumber = parseInt(reverNumber[i]);
+
+      if(i % 2 !==0){
+        newNumber = newNumber * 2
+        if(newNumber > 9){
+
+          newNumber = newNumber - 9;
+          //console.log(newNumber);
+        }
+      }
+      sumaTotal = sumaTotal + newNumber;
+      //console.log(sumaTotal);
+    }
+    return(sumaTotal % 10) === 0;
+  },
+  maskify:(creditCardNumber) => {
+    return creditCardNumber.replace(/.(?=.{4})/g, "#");
+  }
+  
+}
 
 export default validator;
+
+
